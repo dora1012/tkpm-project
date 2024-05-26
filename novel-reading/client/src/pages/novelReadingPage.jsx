eaec220a173bc38e2699c6af664897df4a69e121
dora1012 <lephanxuandung1012@gmail.com>
2024-05-26 23:22:38 +0700

    đưa dữ liệu từ BE lên FE

--- novel-reading/client/src/pages/novelReadingPage.jsx
+++ novel-reading/client/src/pages/novelReadingPage.jsx
@@ -1,20 +1,46 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { novelData } from '../utils/fetchFromAPI';
+// import { novelData } from '../utils/fetchFromAPI';
 import { useNavigate } from 'react-router-dom';
 import { slugify } from '../utils/slugify';
 import SettingPanel from '../components/settingPanel';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const novelReadingPage = () => {
+  
   const extractChapterNumber = (chapterString) => {
     const match = chapterString.match(/\d+$/);
     return match ? parseInt(match[0], 10) : null;
   };
-
+  const [novelData, setNovelData] = useState([]);
   const { slug, chapterNumber } = useParams();
   const navigate = useNavigate();
-  const novel = novelData.find(n => slugify(n.title) === slug);
+  // const novel = novelData.find(n => slugify(n.novelTitle) === slug);
   const currentChapter = parseInt(extractChapterNumber(chapterNumber), 10);
+
+  useEffect(() => {
+    // Fetch novel list from backend
+    const fetchNovelContent = async () => {
+        try {
+            const response = await axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/api-content/' +slug+ '/' +chapterNumber);
+            console.log(import.meta.env.VITE_SERVER_DOMAIN + '/api-content/' + slug + '/' + chapterNumber);
+            // Check if the response is JSON
+            // if (response.headers['content-type'].includes('application/json')) {
+                if (Array.isArray(response.data)) {
+                    setNovelData(response.data);
+                }
+            // } else {
+            //     throw new Error('Response is not JSON');
+            // }
+        } catch (error) {
+            console.error('Error fetching main list:', error);
+        }
+    };
+
+
+    fetchNovelContent();
+}, []);
 
   const [background, setBackground] = useState('white');
   const [fontSize, setFontSize] = useState(20);
@@ -27,9 +53,9 @@ const novelReadingPage = () => {
 
   const textColor = isDarkBackground(background) ? 'text-white' : 'text-black';
 
-  if (!novel) {
-    return <div>Novel not found</div>;
-  }
+  // if (!novel) {
+  //   return <div>Novel not found</div>;
+  // }
 
   if (!currentChapter) {
     return <div>Chapter not found</div>;
@@ -41,21 +67,20 @@ const novelReadingPage = () => {
     }
   };
 
-  const handleNextChapter = () => {
-    if (currentChapter < novel.chapterList.length - 1) {
-      navigate(`/${slug}/chuong-${currentChapter + 1}`);
-    }
-  };
-
+  // const handleNextChapter = () => {
+  //   if (currentChapter < novel.chapterList.length - 1) {
+  //     navigate(`/${slug}/chuong-${currentChapter + 1}`);
+  //   }
+  // };
+  
   return (
     <div className={`container mx-auto p-8 w-full shadow ${textColor}`} style={{ backgroundColor: background, fontFamily: fontStyle }}>
       <div className="flex flex-col items-center justify-center gap-6">
-        <img src={novel.img} alt="Novel Cover" className="h-[310px] w-[220px] object-cover" />
-        <h1 className="text-3xl font-bold mx-auto">{novel.title}</h1>
-        <p className="text-smoke">Tác giả: {novel.author}</p>
+        <h1 className="text-3xl font-bold mx-auto">{novelData.Noveltitle}</h1>
+        <p className="text-smoke">{novelData.chapterTitle}</p>
       </div>
       <div className="w-9/12 border border-grey mx-auto mt-5"></div>
-      <div className="w-1/2 flex justify-between items-center mx-auto mb-4 mt-8">
+      {/* <div className="w-1/2 flex justify-between items-center mx-auto mb-4 mt-8">
         <button
           className="bg-coral-pink text-white py-2 px-4 rounded"
           onClick={handlePreviousChapter}
@@ -81,7 +106,7 @@ const novelReadingPage = () => {
         >
           Chương sau
         </button>
-      </div>
+      </div> */}
       <div className="prose max-w-none w-9/12 mx-auto">
         <div className={`block text-${fontSize}`}>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem porro culpa aspernatur quidem, corporis ducimus, aperiam hic voluptas voluptatibus aliquid impedit repellendus tempora veniam, dolore eos optio deleniti ab odit laudantium! Architecto dicta error obcaecati. Voluptates magni, quae libero ut veritatis ex dolorum alias id ea, tenetur labore minus sequi aperiam perferendis accusantium ullam ad officia facere? Fuga enim similique voluptas. Et, distinctio consequatur, pariatur modi exercitationem possimus unde soluta, eligendi culpa maxime quae ipsum id nesciunt earum sunt dolorum odio vitae eveniet adipisci nemo deserunt nisi molestiae! Tenetur inventore voluptatem ab quae in aut vitae vel voluptatum, deleniti eveniet.</p>
@@ -92,7 +117,7 @@ const novelReadingPage = () => {
         </div>
         
       </div>
-      <div className="w-4/6 flex justify-between items-center mx-auto mb-4 mt-8">
+      {/* <div className="w-4/6 flex justify-between items-center mx-auto mb-4 mt-8">
         <button
           className="bg-coral-pink text-white py-2 px-4 rounded"
           onClick={handlePreviousChapter}
@@ -118,7 +143,7 @@ const novelReadingPage = () => {
         >
           Chương sau
         </button>
-      </div>
+      </div> */}
       <SettingPanel 
         onChangeBackground={setBackground} 
         onChangeFontStyle={setFontStyle} 
--- novel-reading/server/server.js
+++ novel-reading/server/server.js
@@ -1,28 +1,36 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 const hostname = process.env.HOST_NAME || 'localhost';
 
+
 const parserBody = require('body-parser'); // Import the body-parser module
+app.use(cors());
 
 // const novelRoutes = require('./routes/novelRoutes'); // Uncomment this line to import the novelRoutes module
 // const searchRoutes = require('./routes/searchRoutes'); // Uncomment this line to import the searchRoutes module
-const mainListRoutes = require('./routes/mainListRoutes'); 
+const mainListRoutes = require('./routes/mainListRoutes');
 const { getMainList, getNovelListOfMainList } = require('./controllers/mainListController');
 const { get } = require('request-promise');
+const {getChapterContent} = require('./controllers/chapterController')
 
 app.use(parserBody.json());
+
 
 // Use the routes
 // app.use('/api', novelRoutes); // Uncomment this line to use the novelRoutes
 // app.use('/api', searchRoutes); // Uncomment this line to use the searchRoutes
-app.use('/api', mainListRoutes); 
+app.get('/api/danh-sach', getMainList);
+app.get('/api-content/:slug/:chapterNumber', getChapterContent);
 
-// Test endpoint for main list
-// app.get('/test-main-list',getNovelListOfMainList);
 
+
+
+console.log();
 
 // // Test endpoint for novel list of a specific type
 // app.get('/test-novel-list/:listSlug', async (req, res) => {
@@ -38,11 +46,15 @@ app.use('/api', mainListRoutes);
 // });
 
 
+
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ error: 'Something went wrong!' });
 });
+
+
 
 
 app.listen(port, hostname, () => {
