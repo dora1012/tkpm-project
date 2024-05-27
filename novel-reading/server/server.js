fccf50bb0c71c87c3ffd3d6dc74574358789d514
demohcmus <lqngo123@gmail.com>
2024-05-27 09:02:42 +0700

    delete comment

--- novel-reading/server/server.js
+++ novel-reading/server/server.js
@@ -11,6 +11,7 @@ const parserBody = require('body-parser'); // Import the body-parser module
 // const searchRoutes = require('./routes/searchRoutes'); // Uncomment this line to import the searchRoutes module
 const mainListRoutes = require('./routes/mainListRoutes'); 
 const { getMainList, getNovelListOfMainList } = require('./controllers/mainListController');
+const{getChapterContent} = require('./controllers/chapterController');
 const { get } = require('request-promise');
 
 app.use(parserBody.json());
@@ -18,24 +19,14 @@ app.use(parserBody.json());
 // Use the routes
 // app.use('/api', novelRoutes); // Uncomment this line to use the novelRoutes
 // app.use('/api', searchRoutes); // Uncomment this line to use the searchRoutes
-app.use('/api', mainListRoutes); 
+// app.use('/api', mainListRoutes); 
 
 // Test endpoint for main list
-// app.get('/test-main-list',getNovelListOfMainList);
+app.get('/test-main-list',getMainList);
+app.get('/test-chapter-content/:novelSlug/:chapterSlug',getChapterContent);
 
 
-// // Test endpoint for novel list of a specific type
-// app.get('/test-novel-list/:listSlug', async (req, res) => {
-//   try {
-//     const { listSlug } = req.params;
-//     const novels = await getNovelListOfMainList({ params: { listSlug } });
-//     console.log(novels); // Log the novels list to console
-//     res.json(novels); // Send the response as JSON
-//   } catch (error) {
-//     console.error('Error fetching novel list:', error);
-//     res.status(500).json({ error: 'Failed to fetch novel list' });
-//   }
-// });
+
 
 
 // Error handling middleware
