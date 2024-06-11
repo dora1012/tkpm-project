a2a09d90f3c15a621677c0177630925f29f50da8
demohcmus <lqngo123@gmail.com>
2024-06-11 23:09:44 +0700

    add author route

--- novel-reading/server/routes/authorRoutes.js
+++ novel-reading/server/routes/authorRoutes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 // Lấy số trang max của tác giả
 router.get('/:authorSlug/max-trang', authorController.getMaxPaginationNumber);
-// Lấy list truyện của tác giả
+// Lấy list truyện của tác giả theo trang
 router.get('/:authorSlug/:paginationSlug', authorController.getNovelListOfAuthor);
+// Lấy thông tin truyện của tác giả
+router.get('/:authorSlug/', authorController.getNovelListOfAuthor);
 
 module.exports = router;
