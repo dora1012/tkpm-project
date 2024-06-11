b6b2830b2586409004a2ed57dd023153373156a6
demohcmus <lqngo123@gmail.com>
2024-06-11 16:27:49 +0700

    change novel controller for req url and get max pagination

--- novel-reading/server/routes/novelRoutes.js
+++ novel-reading/server/routes/novelRoutes.js
@@ -7,6 +7,7 @@ router.get('/truyen-hot', novelController.getTruyenHot);
 router.get('/truyen-moi-cap-nhat', novelController.getTruyenMoiCapNhat);
 router.get('/truyen-da-hoan-thanh', novelController.getTruyenDaHoanThanh);
 router.get('/:novelSlug/danh-sach-chuong', novelController.getAllChapters);
+router.get('/:novelSlug/max-trang', novelController.getMaxPaginationNumber);
 router.get('/:novelSlug/trang-:paginationSlug',novelController.getChapterListForEachPagination);
 router.get('/:novelSlug/chuong-:chapterSlug', chapterController.getChapter);
 router.get('/:novelSlug', novelController.getNovelInfor);
