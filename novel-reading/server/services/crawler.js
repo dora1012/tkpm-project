b5c7bd0a0906cc02e19b13ad0d30ba399194ae24
Nguyen Tuan Anh <21120410@student.hcmus.edu.vn>
2024-06-13 17:44:20 +0700

    fix crawler

--- novel-reading/server/services/crawler.js
+++ novel-reading/server/services/crawler.js
@@ -1,23 +1,12 @@
 const source = require('../config/sources.js');
-const TruyenFull = require('./crawlerTruyenFull');
-const TangThuVien = require('./crawlerTangThuVien');
 
 class Crawler {
     constructor(strategy) {
         this.strategy = strategy;
     }
 
-    setStrategy(source) {
-        switch (source) {
-            case 'truyenfull':
-                this.strategy = new TruyenFull();
-                break;
-            case 'tangthuvien':
-                this.strategy = new TangThuVien();
-                break;
-            default:
-                throw new Error('Invalid source');
-        }
+    setStrategy(strategy) {
+        this.strategy = strategy;
     }
 
 
